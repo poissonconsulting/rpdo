e7f9d3deda34946fb5f4c0d3df18bb4aa4efd719
joethorley <joe@poissonconsulting.ca>
2015-08-19 15:59:36 -0700

    init

---
+++ data-raw/data-raw.R
@@ -0,0 +1,24 @@
+library(curl)
+library(magrittr)
+library(readr)
+library(tidyr)
+library(dplyr)
+library(devtools)
+
+pdo <- tempfile()
+curl_download("http://research.jisao.washington.edu/pdo/PDO.latest", pdo)
+pdo %<>% readLines()
+
+pdo <- pdo[grepl("(^YEAR)|(^\\d{4,4}[*]{0,2})\\s", pdo)]
+pdo %<>% strsplit(" ")
+pdo %<>% lapply(function (x) x[grepl(".+", x)])
+pdo %<>% sapply(function (x) paste(x, collapse = ","))
+pdo %<>% paste(collapse = "\n")
+pdo %<>% read_csv()
+pdo %<>% gather("Month", "PDO", -YEAR)
+pdo %<>% rename(Year = YEAR)
+pdo %<>% mutate(Month = as.integer(Month))
+pdo %<>% filter(!is.na(PDO))
+
+use_data(pdo, overwrite = TRUE)
+
