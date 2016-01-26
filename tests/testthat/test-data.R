baa7cc715909342dd38d7fdf91b06c1710f2039d
Joe Thorley <joe@poissonconsulting.ca>
2016-01-26 11:45:59 -0800

    Switched to data_check2 in datacheckr v0.2.0

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -2,5 +2,5 @@ context("data")
 
 test_that("pdo", {
   pdo <- rpdo::pdo
-  expect_identical(datacheckr::check_data(pdo, list(Year = c(1900L, 2016L), Month = 1:12, PDO = c(-4, 4))), pdo)
+  expect_identical(datacheckr::check_data2(pdo, list(Year = c(1900L, 2016L), Month = 1:12, PDO = c(-4, 4))), pdo)
 })
