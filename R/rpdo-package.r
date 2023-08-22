d0ccdd47ee0ca56d3f48a596b0e9b54228853301
Joe Thorley <joe@poissonconsulting.ca>
2023-08-22 05:04:49 -0700

    update package documentation

--- R/rpdo-package.r
+++ R/rpdo-package.r
@@ -1,9 +1,6 @@
-#' rpdo
-#'
-#' Monthly Pacific Decadal Oscillation (PDO) index
-#' values from January 1900.
-#'
-#' @seealso [pdo()] and [pdo_download()]
-#' @name rpdo
-#' @docType package
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
 NULL
